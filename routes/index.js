feee4df9809faf37341de4d9f5ed0ec1d1f98739
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-05-29 13:52:53 +0200

    create new page

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
 
+/* GET new page. */
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'New' });
+});
+
 module.exports = router;
